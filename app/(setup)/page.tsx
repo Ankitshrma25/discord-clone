95e5080b97b09f76e150bfa59b0c833e657852d1
Ankit Sharma <ankit302290@gmail.com>
2025-04-28 21:16:36 +0530

    SetUp Initial Modal UI

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -20,7 +20,7 @@ const SetupPage = async () => {
         return redirect(`/servers/${server.id}`);
     }
 
-    return <div>Create a Server</div>;
+    return <InitialModal />;
 }
  
 export default SetupPage;
