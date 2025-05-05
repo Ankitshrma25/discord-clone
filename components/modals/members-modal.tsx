b075eb3647903d38a6a895632c0e887e8d4ecd32
Ankit Sharma <ankit302290@gmail.com>
2025-05-05 20:02:55 +0530

    Started Adding Members Modal UI

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -20,7 +20,7 @@ import { useModal } from "@/hooks/use-modal-store";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useOrigin } from "@/hooks/use-origin";
+
 import axios from "axios";
 
 
@@ -28,8 +28,7 @@ export const MembersModal = () => {
 
     // Modal hook that has the useModal hook which is use for data centralization
     const { onOpen,isOpen, onClose, type, data } = useModal(); 
-    // Hook that provies the current url
-    const origin = useOrigin();  
+    
   
 
     // This is for opening the modal for creating a server
@@ -37,39 +36,7 @@ export const MembersModal = () => {
     // Extract the server from the data
     const { server } = data;
 
-    const [copied, setCopied] = useState(false);
-
-    const [isLoading, SetIsLoading] = useState(false);
-
-
-    // constant to store the invite url
-    const inviteUrl = `${origin}/invite/${server?.inviteCode}`;
-
-
-    // function to copy invite code
-    const onCopy = () => {
-      navigator.clipboard.writeText(inviteUrl);
-      setCopied(true);
-
-      setTimeout(() => {
-        setCopied(false);
-      }, 1000);
-    }
     
-
-    // fucntion to refresh the invite code
-    const onNew = async () => {
-      try {
-        SetIsLoading(true);
-        const response = await axios.patch(`/api/servers/${server?.id}/invite-code`);
-        onOpen("invite", { server: response.data });
-      } catch (error) {
-        console.log(error);
-      } finally {
-        SetIsLoading(false);
-      }
-    }
-
     
     return (
 <>
@@ -85,35 +52,7 @@ export const MembersModal = () => {
             </DialogTitle>
           </DialogHeader>
          <div className="p-6">
-          <Label
-            className="text-xs font-bold uppercase tracking-widest text-zinc-500 dark:text-secondary/70"
-          >
-            Server Invite Link
-          </Label>
-          <div className="flex items-center mt-2 gap-x-2">
-            <Input 
-            disabled={isLoading}
-              className="border-0 bg-zinc-300/50 text-black focus-visible:ring-0 focus-visible:ring-offset-0"
-              value={inviteUrl}
-            />
-            <Button disabled={isLoading} onClick={onCopy} size="icon">
-              {copied 
-              ? <Check className="h-4 w-4" /> 
-              :  <Copy className="h-4 w-4" />
-              }
-             
-            </Button>
-          </div>
-          <Button
-          disabled={isLoading}
-          onClick={onNew}
-          variant="link"
-          size="sm"
-            className="mt-4 w-full cursor-pointer text-xs tracking-wider text-zinc-800"
-          >
-            Generate a new link
-            <RefreshCw className="h-4 w-4 ml-2" />
-          </Button>
+            Hello Members
          </div>
         </DialogContent>
       </Dialog>
