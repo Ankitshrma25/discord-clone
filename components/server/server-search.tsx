74dd0f2fb0869d74db7d678ae4214c94aef384ba
Ankit Sharma <ankit302290@gmail.com>
2025-05-07 15:32:36 +0530

    Search server Modal Done

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -4,14 +4,15 @@
 
 import { Search } from "lucide-react";
 import { useEffect, useState } from "react";
-import { 
-    CommandDialog, 
-    CommandEmpty, 
-    CommandGroup, 
-    CommandInput, 
-    CommandItem, 
+import {
+    CommandDialog,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
     CommandList
 } from "@/components/ui/command";
+import { useParams, useRouter } from "next/navigation";
 
 interface ServerSearchProps {
     data: {
@@ -31,6 +32,10 @@ export const ServerSearch = ({
     // state for opening the server search
     const [open, setOpen] = useState(false);
 
+    const router = useRouter();
+
+    const params = useParams();
+
     useEffect(() => {
         const down = (e: KeyboardEvent) => {
             if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
@@ -42,20 +47,31 @@ export const ServerSearch = ({
         return () => document.removeEventListener("keydown", down);
     }, []);
 
+    //onclick function
+    const onClick = ({ id, type }: { id: string; type: "channel" | "member" }) => {
+        setOpen(false);
+        if (type === "member") {
+            return router.push(`/servers/${params?.serverId}/conversations/${id}`);
+        }
+        if (type === "channel") {
+            return router.push(`/servers/${params?.serverId}/channels/${id}`);
+        }
+    }
+
     return (
         <>
             <button
-            onClick={() => setOpen(true)}
-            className="group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
+                onClick={() => setOpen(true)}
+                className="group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
             >
                 <Search className="w-4 h-4 text-zinc-500 dark:text-zinc-400" />
                 <p
-                className="font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition"
+                    className="font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition"
                 >
                     Search
                 </p>
                 <kbd
-                className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground ml-auto"
+                    className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground ml-auto"
                 >
                     <span className="text-sm">ctrl</span>K
                 </kbd>
@@ -72,9 +88,9 @@ export const ServerSearch = ({
                                 {data.map(({ id, icon, name }) => {
                                     return (
                                         <CommandItem
-                                        key={id}
-                                        value={name}
-                                        onSelect={() => console.log(`Selected ${name}`)}
+                                            key={id}
+                                            onSelect={() => onClick({ id, type })}
+                                            className="text-sm"
                                         >
                                             {icon}
                                             <span>{name}</span>
